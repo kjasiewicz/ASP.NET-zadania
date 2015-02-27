e2400c648874fc9baefe7c807a365476f78892c4
kjasiewicz <katarzynajasiewicz.93@gmail.com>
2015-02-27 14:26:32 +0100

    zajęcia01

---
+++ zadanie1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using zadanie1.Models;
+
+namespace zadanie1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var root = XElement.Load("http://news.google.pl/news?pz=1&cf=all&ned=pl_pl&hl=pl&output=rss");
+            return View(root.Descendants("item").Select(x=>
+                new RssItem
+                {
+                    Description=x.Element("description").Value,
+                    Title=x.Element("title").Value
+                }).ToList());
+        }
+        public ActionResult vb()
+        {
+            var root = XElement.Load("http://news.google.pl/news?pz=1&cf=all&ned=pl_pl&hl=pl&output=rss");
+            ViewBag.vb = root.Descendants("item").Select(x =>
+                new RssItem
+                {
+                    Description = x.Element("description").Value,
+                    Title = x.Element("title").Value
+                }).ToList();
+            return View();
+
+        }
+    }
+}
---
+++ zadanie1/Models/RssItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace zadanie1.Models
+{
+    public class RssItem
+    {
+        [Display(Name="tytuł")]
+        
+        public string Title { get; set; }
+        [UIHint("html")]
+        [Display(Name="opis")]
+        public string Description { get; set; }
+    }
+}
